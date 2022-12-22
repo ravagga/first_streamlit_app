508237ee9f5268072f31a33a9d164558b4660160
ravagga <42155546+ravagga@users.noreply.github.com>
2022-12-22 19:51:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
      streamlit.error()
       
 #don't run anything past here until we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 #Snowflake related functions
